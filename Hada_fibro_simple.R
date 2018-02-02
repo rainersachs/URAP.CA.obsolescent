c5e5765c33166ba4438e5cfc9ed49f5396dd355f
AndyZhaoly <andy.zhaoly@berkeley.edu>
2018-02-02 12:07:50 -0800

    Update Hada_fibro_simple.R

--- Hada_fibro_simple.R
+++ Hada_fibro_simple.R
@@ -12,11 +12,13 @@ rm(list=ls())
 
 #Create dataframes that store the fibroblast WGE simple CA data used in 16Cacao 
 
-Oxygen = data.frame(d = c(0, .0125, .02, .025, .05, .075, .1, .2, .4),CA = c(.24, 1.66, 2.43, 2.37, 1.16, 2.85, 2.58, 6.94, 6.91))# Some GCR components are high-speed Oxygen nuclei that are almost fully ionized. d=dose; CA are per hundred cells.
+Oxygen = data.frame(d = c(0, .0125, .02, .025, .05, .075, .1, .2, .4),CA = c(.24, 1.66, 2.43, 2.37, 1.16, 2.85, 2.58, 6.94, 6.91))
+# Some GCR components are high-speed Oxygen nuclei that are almost fully ionized. d=dose; CA are per hundred cells.
 
 Si = data.frame(d = c(0, .02, .04, .06, .08, .1, .12, .2, .4, .8, 1.2), CA = c(.11, 1.26, 1.14, 1.58, 1.22, 1.89, 3.47, 4.6, 9.79, 27.01, 38.84))
 
-Fe600 = data.frame(d = c(0, .01, .02, .04, .06, .08, .1, .12, .2, .4, .8), CA = c(.13, .76, .99, 1.2, 1.74, 1.28, 1.2, 1.7, 3.02, 5.52, 12.42)) #600 refers to the energy in MeV per atomic mass unit in this Iron beam
+Fe600 = data.frame(d = c(0, .01, .02, .04, .06, .08, .1, .12, .2, .4, .8), CA = c(.13, .76, .99, 1.2, 1.74, 1.28, 1.2, 1.7, 3.02, 5.52, 12.42)) 
+#600 refers to the energy in MeV per atomic mass unit in this Iron beam
 
 Fe450 = data.frame(d = c(0, .02, .04, .06, .08, .1, .2, .4), CA = c(0, .86, .6, .8, 1.22, 2.02, 2.3, 4.77))
 
@@ -33,7 +35,7 @@ big_df = rbind(Oxygen, Si, Ti, Fe600, Fe450, Fe300)
 big_df$Z = rep(param$Z, times = c(9, 11, 9, 11, 8, 10))
 big_df$Z.b = rep(param$Z.b, times = c(9, 11, 9, 11, 8, 10))
 big_df$L = rep(param$L, times = c(9, 11, 9, 11, 8, 10))
-big_df$error = c(0.24, 0.63, 0.77, 0.75, 0.52, 0.82, 0.78, 1.31, 1.59, 0.12, 0.05, 0.07, 0.56, 0.18, 0.60, 1.23, 1.60, 1.55, 4.27, 7.21, 0, 0.70, 0.66, 0.59, 0.80, 0.78, 0.48, 1.15, 2.39, 0.16, 0.38, 0.24, 0.21, 0.02, 0.37, 0.54, 0.17, 0.55, 1.75, 2.59, 0, 0.43, 0.34, 0.40, 0.50, 0.64, 0.73, 1.09, 0.29, 0.55, 0.60, 0.55, 0.49, 0.60, 0.54, 1.03, 1.22, 3.62) #error bars for the CA measurements
+big_df$error = c(0.24, 0.63, 0.77, 0.75, 0.52, 0.82, 0.78, 1.31, 1.59, 0.12, 0.05, 0.07, 0.56, 0.18, 0.60, 1.23, 1.60, 1.55, 4.27, 7.21, 0, 0.70, 0.66, 0.59, 0.80, 0.78, 0.48, 1.15, 2.39, 0.16, 0.38, 0.24, 0.21, 0.43, 0.37, 0.54, 0.17, 0.55, 1.75, 2.59, 0, 0.43, 0.34, 0.40, 0.50, 0.64, 0.73, 1.09, 0.29, 0.55, 0.60, 0.55, 0.49, 0.60, 0.54, 1.03, 1.22, 3.62) #error bars for the CA measurements
 big_df$ion = rep(param$ion, times = c(9, 11, 9, 11, 8, 10))
 
 #Next modify the data frame to get rid of the zero dose points. Background CA frequency was determined seperately.
@@ -61,20 +63,27 @@ NTE2_function = function(d, L, Z.b, eta0 = 0.00047, eta1 = 0.011, sig0 = 6.75, k
 
 #Our IDERs (Individual Dose Effect Relations). Applicable to the 1-ion components of a mixed simulated GCR beam 
 #Modifying NTE1 and NTE2 by insisting they be twice continuously differentiable and monotonic increasing. Double check NTE1, NTE2, Our model
-IDER = function(d, L, Z.b, eta0, eta1, sig0, kap) {
-  P = (1-exp(-Z.b/kap))^2
-  sig = sig0*P + 0.041/6.24*L*(1-P) # 0.041 +- 0.0051 comes from 16Cacao
-  eta = eta0*L*exp(-eta1*L)
+IDER = function(d, L, Z.b, eta00, eta10, sig00, kap0) {
+  P = (1-exp(-Z.b/kap0))^2
+  sig = sig00*P + 0.041/6.24*L*(1-P) # 0.041 +- 0.0051 comes from 16Cacao
+  eta = eta00*L*exp(-eta10*L)
   0.00071 + sig*6.24*d/L*(1-exp(-1024*d/L)) + eta*(1-exp(-10^5*d))  #0.00071 + sig*6.24*d/L*(1-exp(-1024*d/L)) + eta*(1-exp(-10^3*d))#don't use
 } 
 
 #nls (non-linear least square) method to get the parameters needed (4 parameter estimation) #rks to laz: see note under issues
-IDER_model = nlsLM(CA ~ IDER(d, L, Z.b, eta0, eta1, sig0, kap), data = modified_df, start = list(eta0 = 0.001, eta1 = 0.01, sig0 = 5, kap = 500), 
+IDER_model = nlsLM(CA ~ IDER(d, L, Z.b, eta00, eta10, sig00, kap0), data = modified_df, start = list(eta00 = 0.001, eta10 = 0.01, sig00 = 5, kap0 = 500), 
                    weights = (1/(modified_df$error)^2))
 coef(IDER_model)
 #e.g. eta0 = 1.484687e-04, eta1 = 3.513927e-03, sig0 = 4.149660e+00, kap = 4.688287e+02
 vcov(IDER_model)# variance-covariance matrix, needed later for analyzing 95% confidence limits in baseline no-synergy/no-antagonism MIXDER (Mixture dose effect relation)
 summary(IDER_model, cor = TRUE) #model parameters and their correlation matrix
+
+XX <- coef(IDER_model)
+eta00 <- as.numeric(XX[1])
+eta10 <- as.numeric(XX[2])
+sig00 <- as.numeric(XX[3])
+kap0 <- as.numeric(XX[4])
+
 
 #R function to give Information criteria (AIC and BIC) #rks to laz: R has AIC functions. Please check that they give the same ordering (not necessarily the same values) as the ones constructed below by Dae or you.
 #L_function gives the residuals squared
@@ -87,7 +96,7 @@ L_function = function(func, eta0, eta1, sig0, kap) {
 }
 L_NTE1 = L_function(NTE1_function, eta0 = 0.00011, eta1 = 0.007, sig0 = 6.12, kap = 796)
 L_NTE2 = L_function(NTE2_function, eta0 = 0.00047, eta1 = 0.011, sig0 = 6.75, kap = 590)
-L_IDER = L_function(IDER, eta0 = 1.484687e-04, eta1 = 3.513927e-03, sig0 = 4.149660e+00, kap = 4.688287e+02)
+L_IDER = L_function(IDER, eta0 = eta00, eta1 = eta10, sig0 = sig00, kap = kap0)
 
 #Since all models are weighted least square regression, will weight our model with our weights to get the WRSS (weighted residual squared sum)
 WRSS_NTE1 = sum((1/modified_df$error^2)*L_NTE1)
@@ -113,7 +122,7 @@ information_critera_df = data.frame(AIC = c(NTE1_AIC, NTE2_AIC, IDER_AIC), BIC =
 information_critera_df ## RKS runs well up to here. Lots of checks made 8/12/2017
 
 # #Baseline no-synergy/no-antagonism MIXDER  based on any input
-MIXDER_function = function(r, L, Z.b, d = seq(0, 0.2, by = 0.001), eta0 = 1.484687e-04, eta1 = 3.513927e-03, sig0 = 4.149660e+00, kap = 4.688287e+02) {
+MIXDER_function = function(r, L, Z.b, d = seq(0, 0.2, by = 0.001), eta0 = eta00, eta1 = eta10, sig0 = sig00 ,kap = kap0) {
   dE=function(yini,State,Pars){
     eta0 = eta0; eta1 = eta1; sig0 = sig0; kap = kap
     with(as.list(c(State, Pars)), {
@@ -140,10 +149,11 @@ MIXDER_function = function(r, L, Z.b, d = seq(0, 0.2, by = 0.001), eta0 = 1.4846
   return(out)
 }
 
+
 #Graphs for NTE1 and our IDER models: 12_ion figure
 #When creating any sort of MIXDER figures we use this IDER now without the background effect then add the background effect at the end.
-IDER = function(d, L, Z.b, eta0 = 1.484687e-04, eta1 = 3.513927e-03, sig0 = 4.149660e+00, kap = 4.688287e+02) {
-  P = (1-exp(-Z.b/kap))^2
+IDER = function(d, L, Z.b, eta0 = eta00, eta1 = eta10, sig0 = sig00, kap = kap0) {
+  P = (1-exp(-Z.b/kap0))^2
   sig = sig0*P + 0.041/6.24*L*(1-P)
   eta = eta0*L*exp(-eta1*L)
   sig*6.24*d/L*(1-exp(-1024*d/L)) + eta*(1-exp(-10^5*d))#sig*6.24*d/L*(1-exp(-1024*d/L)) + eta*(1-exp(-.5*10^3*d))#don't use
@@ -157,6 +167,7 @@ SEA=function(d){
   IDER(d/6,L[1],Z.b[1])+IDER(d/6,L[2],Z.b[2])+IDER(d/6,L[3],Z.b[3])+IDER(d/6,L[4],Z.b[4])+IDER(d/6,L[5],Z.b[5])+IDER(d/6,L[6],Z.b[6])
 }
 lines(dose,SEA(dose),lty=2)
+
 #lines(dose,sum(SEA))
 #Comparing the NTE1 model in 16Cacao with our NASA SNTE model
 
@@ -166,7 +177,6 @@ lines(dose,SEA(dose),lty=2)
 # d_2 = c(0, 1e-6*(1:9), 1e-5*(1:9), 1e-4*(1:9), 1e-3*(1:300))
 # plot(c(0,.3),c(0,0.04), bty='l', ann='F')
 # lines(d_2,IDER(d_2,240,1585), bty='l', ann='F')
-# #setEPS()
 # postscript("NTE1_model test.eps", width = 5, height = 7)
 # par(mfrow=c(2,1))
 # 
@@ -179,16 +189,20 @@ lines(dose,SEA(dose),lty=2)
 # arrows(big_df$d[1:7]*100, big_df$errorbar_lower[1:7]*100, big_df$d[1:7]*100, big_df$errorbar_upper[1:7]*100, length=0.02, angle=90, code=3)
 # lines(x = d_1*100, y = 100*(0.00071 + IDER(d = d_1, L = 75, Z.b = 595)), col = "red")
 # lines(x = d_1*100, y = 100 * NTE1_function(d = d_1, L= 75, Z.b = 595), col = "blue", lty = 2)
-# 
-# dev.off()
+
+
+
+
+#Monte Carlo
 sig = vcov(IDER_model)
 set.seed(10)
-monte_carlo_parameters = rmvnorm(n = 500, mean = c(eta0 = 1.484687e-04, eta1 = 3.513927e-03, sig0 = 4.149660e+00, kap = 4.688287e+02), sigma = sig)
+monte_carlo_parameters = rmvnorm(n = 500, mean = c(eta0 = eta00, eta1 = eta10, sig0 = sig00, kap = kap0), sigma = sig)
 eta0_MC = monte_carlo_parameters[, 1]
 eta1_MC = monte_carlo_parameters[, 2]
 sig0_MC = monte_carlo_parameters[, 3]
 kap_MC = monte_carlo_parameters[, 4]
 kap_MC[kap_MC <= 1e-6] = 1e-5 
+
 #note because our last parameter were less significant when inputting our variance covariance matrix the parameters had to actually be readjusted to fix for negative value. This is important because if kap parameter went negative our model is nonsensical. (Luckily the fix was only for exactly 15 out of 500 MC samples or only roughly 3%)
 
 #This is a general CI_function where you just get a CI for a specific dose point for any synergy analysis using the monte carlo simulations using vcov().
@@ -210,10 +224,10 @@ CI_function_MIXDER = function(d, d_interested, r, interval = 0.95, L, Z.beta) {
 
 #Monte Carlo using the error bars from summary()
 set.seed(11)
-eta0_MC_2 = rnorm(1000, mean = 1.485e-04, sd = 2.190e-05)
-eta1_MC_2 = rnorm(1000, mean = 3.514e-03, sd = 9.037e-04)
-sig0_MC_2 = rnorm(1000, mean = 4.150e+00, sd = 1.366e+00)
-kap_MC_2 = rnorm(1000, mean = 4.688e+02, sd = 2.469e+02)
+eta0_MC_2 = rnorm(1000, mean = eta00, sd = 4.631e-05)
+eta1_MC_2 = rnorm(1000, mean = eta10, sd = 1.617e-04)
+sig0_MC_2 = rnorm(1000, mean = sig00, sd = 1.966e+00)
+kap_MC_2 = rnorm(1000, mean = kap0, sd = 2.628e+02)
 kap_MC_2[kap_MC_2 <= 1e-6] = 1e-5 
 
 CI_function_MIXDER_MC_2 = function(d, d_interested, r, interval = 0.95, L, Z.beta) {
@@ -234,6 +248,8 @@ CI_function_MIXDER_MC_2 = function(d, d_interested, r, interval = 0.95, L, Z.bet
 out = MIXDER_function(r = rep(1/2, times = 2), L = c(100, 175), Z.b = c(690, 1075), d = c(seq(0, 0.01, 0.001), seq(0.01, 0.5, by = 0.01)))
 two_ion_MIXDER = data.frame(d = out[, 1], CA = out[, 2] + 0.00071)
 d = two_ion_MIXDER$d
+
+
 #Then the 95% CI multivariate MC simulated ribbon
 ninty_five_CI_lower = vector(length = 0)
 ninty_five_CI_upper = vector(length = 0)
@@ -260,7 +276,7 @@ silicon1 <- two_ion_MIXDER$silicon
 ironsix1 <- two_ion_MIXDER$ironsix
 plot(x = d1 * 100, y = CA1 * 100, type = "l", col = "red")
 lines(x = d1 * 100, y = simpleeffect1 * 100, col = "black", lty = 2, lwd = 0.5) + 
-lines(x = d1 * 100, y = silicon1* 100, col = "green")  
+  lines(x = d1 * 100, y = silicon1* 100, col = "green")  
 lines(x = d1 * 100, y = ironsix1* 100, col = "green")
 lines(x= d1*100 , y = two_ion_MIXDER$CI_upper * 100, lty = 'dashed', col = 'red')
 lines(x= d1*100 , y = two_ion_MIXDER$CI_lower * 100, lty = 'dashed', col = 'red')
@@ -341,6 +357,8 @@ lines(x = d2 * 100, y = oxygen2* 100, col = "green")
 lines(x= d2*100 , y = six_ion_MIXDER$CI_upper_two * 100, lty = 'dashed', col = 'red')
 lines(x= d2*100 , y = six_ion_MIXDER$CI_lower_two * 100, lty = 'dashed', col = 'red')
 polygon(c(d2*100,rev(d2*100)),c(six_ion_MIXDER$CI_lower_two * 100, rev(six_ion_MIXDER$CI_upper_two * 100)),col = rgb(1, 0, 0,0.5), border = NA)
+
+
 
 
 
