14e4915a98ef4c6bfc1bc83bad74ab634472170b
Peter Wang <peter.wang@berkeley.edu>
2018-04-08 00:37:38 -0700

    Finished graphs.R
    For the 6-ion models, I added the 10-dose version and 10-dose no covariance 6-ion version. Added titles and subtitles for each plot. The 10-dose no-covariance Monte Carlo data is now available in folder as well (so a total of 6 models, 4 csv files as the fewer-dosage-points models do not require a separate Monte Carlo)
    
    Added the no-covariance model to the  Rmd file. Minor improvements of the Rmd file.

--- Graphs/graphs.R
+++ Graphs/graphs.R
@@ -2,11 +2,11 @@
 library(deSolve) # package for solving differential equations
 library(minpack.lm) # package for non-linear regression #rks to laz: I think we probably can just use nls() in stats, not nlsLM from linpack. Please check in R documentation if there is any functional difference at all
 library(mvtnorm) # package for calculating confidence intervals by Monte Carlo simulation based on variance-covariance matrices #rks to laz: I added to comment.Please check that my addition is OK.
-rm(list=ls())
+#rm(list=ls())
 set.seed(19970101)
 
 ####2-ion 60 dose
-d = c(seq(0, 0.009, 0.001), seq(0.01, 0.5, by = 0.01))
+d_2ion = c(seq(0, 0.009, 0.001), seq(0.01, 0.5, by = 0.01))
 #Assuming the working directory is the location of this R file (On Github the csv file is in the same location as this R file)
 two_ion_MIXDER = read.csv("2ionMonteCarlo.csv") #This is the Monte Carlo results with seed 19970101
 
@@ -16,7 +16,8 @@ CA1 <- two_ion_MIXDER$CA
 simpleeffect1 <- two_ion_MIXDER$simpleeffect
 silicon1 <- two_ion_MIXDER$silicon
 ironsix1 <- two_ion_MIXDER$ironsix
-plot(x = d1 * 100, y = CA1 * 100, type = "l", col = "red")
+plot(x = d1 * 100, y = CA1 * 100, type = "l", col = "red", main="2-ion Model", sub="Full 60 Doses", 
+     xlab="Dose * 100", ylab="CA * 100")
 lines(x = d1 * 100, y = simpleeffect1 * 100, col = "black", lty = 2, lwd = 0.5)
 lines(x = d1 * 100, y = silicon1* 100, col = "green")  
 lines(x = d1 * 100, y = ironsix1* 100, col = "green")
@@ -29,7 +30,7 @@ polygon(c(d1*100,rev(d1*100)),c(two_ion_MIXDER$CI_lower * 100, rev(two_ion_MIXDE
 
 
 ####2-ion 12 dose
-d_small = d[seq(1,60, by = 5)]
+d_2ion_small = d[seq(1,60, by = 5)]
 two_ion_MIXDER_small <- two_ion_MIXDER[seq(1,60, by = 5), ]
 
 #The graphing part
@@ -38,7 +39,8 @@ CA2 <- two_ion_MIXDER_small$CA
 simpleeffect2 <- two_ion_MIXDER_small$simpleeffect
 silicon2 <- two_ion_MIXDER_small$silicon
 ironsix2 <- two_ion_MIXDER_small$ironsix
-plot(x = d2 * 100, y = CA2 * 100, type = "l", col = "red")
+plot(x = d2 * 100, y = CA2 * 100, type = "l", col = "red", main="2-ion Model", sub="12 Doses", 
+     xlab="Dose * 100", ylab="CA * 100")
 lines(x = d2 * 100, y = simpleeffect2 * 100, col = "black", lty = 2, lwd = 0.5)
 lines(x = d2 * 100, y = silicon2* 100, col = "green")  
 lines(x = d2 * 100, y = ironsix2* 100, col = "green")
@@ -53,7 +55,8 @@ polygon(c(d2*100,rev(d2*100)),c(two_ion_MIXDER_small$CI_lower * 100, rev(two_ion
 two_ion_MIXDER_small_var <- read.csv("2ionMonteCarlovar.csv")
 
 #The graphing part
-plot(x = d2 * 100, y = CA2 * 100, type = "l", col = "red")
+plot(x = d2 * 100, y = CA2 * 100, type = "l", col = "red", main="2-ion Model", sub="12 Doses without Covariances", 
+     xlab="Dose * 100", ylab="CA * 100")
 lines(x = d2 * 100, y = simpleeffect2 * 100, col = "black", lty = 2, lwd = 0.5)
 lines(x = d2 * 100, y = silicon2* 100, col = "green")  
 lines(x = d2 * 100, y = ironsix2* 100, col = "green")
@@ -62,7 +65,8 @@ lines(x= d2*100 , y = two_ion_MIXDER_small_var$CI_lower * 100, lty = 'dashed', c
 polygon(c(d2*100,rev(d2*100)),c(two_ion_MIXDER_small_var$CI_lower * 100, rev(two_ion_MIXDER_small_var$CI_upper * 100)),col = rgb(1, 0, 0,0.5), border = NA)
 
 
-####6-ion 60 dose 
+####6-ion 50 dose
+d_6ion <- c(seq(0, 0.009, 0.001), seq(0.01, 0.4, by = 0.01))
 six_ion_MIXDER <- read.csv("6ionMonteCarlo.csv")
 
 #the graphing part
@@ -75,7 +79,8 @@ ironthree11 <- six_ion_MIXDER$ironthree
 ironfour11 <- six_ion_MIXDER$ironfour
 ironsix11 <- six_ion_MIXDER$ironsix
 oxygen11 <- six_ion_MIXDER$oxygen
-plot(x = d11 * 100, y = CA11 * 100, type = "l", col = "red")
+plot(x = d11 * 100, y = CA11 * 100, type = "l", col = "red", main="6-ion Model", sub="Full 50 Doses", 
+     xlab="Dose * 100", ylab="CA * 100")
 lines(x = d11 * 100, y = simpleeffect11 * 100, col = "black", lty = 2, lwd = 0.5)
 lines(x = d11 * 100, y = silicon11* 100, col = "green")  
 lines(x = d11 * 100, y = titanium11* 100, col = "green")
@@ -86,3 +91,56 @@ lines(x = d11 * 100, y = oxygen11* 100, col = "green")
 lines(x= d11*100 , y = six_ion_MIXDER$CI_upper * 100, lty = 'dashed', col = 'red')
 lines(x= d11*100 , y = six_ion_MIXDER$CI_lower * 100, lty = 'dashed', col = 'red')
 polygon(c(d11*100,rev(d11*100)),c(six_ion_MIXDER$CI_lower * 100, rev(six_ion_MIXDER$CI_upper * 100)),col = rgb(1, 0, 0,0.5), border = NA)
+
+####6-ion 10 dose
+d_6ion_small <- d_6ion[seq(1, 50, by = 5)] 
+six_ion_MIXDER_small <- six_ion_MIXDER[seq(1, 50, by = 5),]
+
+#the graphing part
+d22 <- six_ion_MIXDER_small$d
+CA22 <- six_ion_MIXDER_small$CA
+simpleeffect22 <- six_ion_MIXDER_small$simpleeffect
+silicon22 <- six_ion_MIXDER_small$silicon
+titanium22 <- six_ion_MIXDER_small$titanium
+ironthree22 <- six_ion_MIXDER_small$ironthree
+ironfour22 <- six_ion_MIXDER_small$ironfour
+ironsix22 <- six_ion_MIXDER_small$ironsix
+oxygen22 <- six_ion_MIXDER_small$oxygen
+plot(x = d22 * 100, y = CA22 * 100, type = "l", col = "red", main="6-ion Model", sub="10 Doses", 
+     xlab="Dose * 100", ylab="CA * 100")
+lines(x = d22 * 100, y = simpleeffect22 * 100, col = "black", lty = 2, lwd = 0.5)
+lines(x = d22 * 100, y = silicon22* 100, col = "green")  
+lines(x = d22 * 100, y = titanium22* 100, col = "green")
+lines(x = d22 * 100, y = ironthree22* 100, col = "green")  
+lines(x = d22 * 100, y = ironfour22* 100, col = "green")
+lines(x = d22 * 100, y = ironsix22* 100, col = "green")  
+lines(x = d22 * 100, y = oxygen22* 100, col = "green")
+lines(x= d22*100 , y = six_ion_MIXDER_small$CI_upper * 100, lty = 'dashed', col = 'red')
+lines(x= d22*100 , y = six_ion_MIXDER_small$CI_lower * 100, lty = 'dashed', col = 'red')
+polygon(c(d22*100,rev(d22*100)),c(six_ion_MIXDER_small$CI_lower * 100, rev(six_ion_MIXDER_small$CI_upper * 100)),col = rgb(1, 0, 0,0.5), border = NA)
+
+####6-ion 10 dose without covariances (larger Ci's)
+six_ion_MIXDER_small_var <- read.csv("6ionMonteCarlovar.csv")
+
+#the graphing part
+d22 <- six_ion_MIXDER_small_var$d
+CA22 <- six_ion_MIXDER_small_var$CA
+simpleeffect22 <- six_ion_MIXDER_small_var$simpleeffect
+silicon22 <- six_ion_MIXDER_small_var$silicon
+titanium22 <- six_ion_MIXDER_small_var$titanium
+ironthree22 <- six_ion_MIXDER_small_var$ironthree
+ironfour22 <- six_ion_MIXDER_small_var$ironfour
+ironsix22 <- six_ion_MIXDER_small_var$ironsix
+oxygen22 <- six_ion_MIXDER_small_var$oxygen
+plot(x = d22 * 100, y = CA22 * 100, type = "l", col = "red", main="6-ion Model", sub="10 Doses without Covariances", 
+     xlab="Dose * 100", ylab="CA * 100")
+lines(x = d22 * 100, y = simpleeffect22 * 100, col = "black", lty = 2, lwd = 0.5)
+lines(x = d22 * 100, y = silicon22* 100, col = "green")  
+lines(x = d22 * 100, y = titanium22* 100, col = "green")
+lines(x = d22 * 100, y = ironthree22* 100, col = "green")  
+lines(x = d22 * 100, y = ironfour22* 100, col = "green")
+lines(x = d22 * 100, y = ironsix22* 100, col = "green")  
+lines(x = d22 * 100, y = oxygen22* 100, col = "green")
+lines(x= d22*100 , y = six_ion_MIXDER_small_var$CI_upper * 100, lty = 'dashed', col = 'red')
+lines(x= d22*100 , y = six_ion_MIXDER_small_var$CI_lower * 100, lty = 'dashed', col = 'red')
+polygon(c(d22*100,rev(d22*100)),c(six_ion_MIXDER_small_var$CI_lower * 100, rev(six_ion_MIXDER_small_var$CI_upper * 100)),col = rgb(1, 0, 0,0.5), border = NA)
