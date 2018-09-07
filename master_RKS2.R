97dc4075118380362fad849584438370bf9bc73f
Peter Wang <peter.wang@berkeley.edu>
2018-09-06 17:45:31 -0700

    New Monte-Carlo Function
    Added function: monte_carlo that takes in ion names and outputs graph/data (cov = T by default)
    
    Also changed IDER to make it compatible to the new function meanwhile making sure the old codes still work.

--- master_RKS2.R
+++ master_RKS2.R
@@ -2,6 +2,7 @@ library(deSolve) # package for solving differential equations
 library(minpack.lm) # package for non-linear regression #rks to laz: I think we probably can just use nls() in stats, not nlsLM from linpack. Please check in R documentation if there is any functional difference at all
 library(mvtnorm) # package for calculating confidence intervals by Monte Carlo simulation based on variance-covariance matrices #rks to laz: I added to comment.Please check that my addition is OK.
 library(Hmisc)
+library(dplyr)
 rm(list=ls())
 
 #Create dataframes that store the fibroblast WGE simple CA data used in 16Cacao 
@@ -134,11 +135,25 @@ r=rep(1/6,6);L = c(75, 100, 125, 175, 195, 240); Z.b = c(595, 690, 770, 1075, 12
 
 MX=MIXDER_function(r, L, Z.b, d=dose) #for this mixture can't go much above 0.6#
 
-IDER = function(d, L, Z.b, eta0 = eta0_hat, eta1 = eta1_hat, sig0 = sig0_hat, kap = kap_hat) {
+IDER = function(d, L = NULL, Z.b = NULL, ions = NULL, eta0 = eta0_hat, eta1 = eta1_hat, sig0 = sig0_hat, kap = kap_hat) {
+  #SCW: Added an option for inputting ion names instead of L, Z.b to decrease chance of transcription error and keep track of which ions are being mixed. (Old code should still work for now, but ideally we will change everything to this format in the future)
+  #ions is a vector of strings of the names of the ions
+  if (is.null(ions)){
   P = (1-exp(-Z.b/kap_hat))^2
   sig = sig0*P + 0.041/6.24*L*(1-P)
   eta = eta0*L*exp(-eta1*L)
-  sig*6.24*d/L*(1-exp(-1024*d/L)) + eta*(1-exp(-10^5*d))
+  return(sig*6.24*d/L*(1-exp(-1024*d/L)) + eta*(1-exp(-10^5*d)))
+  }
+  else{#After testing, this results in the same output as the old one besides rounding error that cannot be seen in the available digits
+    r = 1/length(ions)
+    info_table = modified_df %>% group_by(ion, L, Z.b) %>% summarise()
+    info_table = suppressWarnings(left_join(data.frame(ions), info_table, by = c("ions" = "ion")))
+    output = 0
+    for (i in 1: nrow(info_table)){
+      output = output + IDER(d*r, L = info_table$L[i], Z.b = info_table$Z.b[i])
+    }
+    return(output)
+  }
 }
 #Plotting
 plot(MX[,1],MX[,2],type='l',bty='l',col='red',ann='F',ylim=c(0,.10), main="CA vs Dose", xlab="Dose", ylab="CA")
@@ -207,6 +222,91 @@ refactor_CI_function_MIXDER_var = function(d, d_interested, r, interval = 0.95, 
 
 ################### END REFACTORING
 
+monte_carlo <- function(ions, r = rep(1/length(ions), length(ions)), d = c(seq(0, 0.009, 0.001), seq(0.01, 0.5, by = 0.01)), eta0 = eta0_hat, eta1 = eta1_hat, sig0 = sig0_hat, kap = kap_hat, n = 500, graph = T, background = 0.00071, cov = T){
+  ##SCW: This is a generic function that takes names of the ions in the desired mixture as input and either plots out the ribbon graph or outputs the dataset
+  #r is a vector that sums up to 1
+  #ions is a vector of strings of the names of the ions in the mixture
+  #d is a vector of different dosage
+  #If cov = F plots out the result without using the cov matrix
+  info_table = modified_df %>% group_by(ion, L, Z.b) %>% summarise() %>% filter(ion %in% ions)
+  info_table = suppressWarnings(left_join(data.frame(ions), info_table, by = c("ions" = "ion")))
+  L = info_table$L
+  Z.b = info_table$Z.b
+  out = MIXDER_function(r = r, L = L, Z.b = Z.b, d = d, eta0 = eta0, eta1=eta1, sig0=sig0, kap = kap)
+  MIXDER = data.frame(d = d, CA = out[, 2] + background)
+  ninty_five_CI_lower = vector(length = length(d))
+  ninty_five_CI_upper = vector(length = length(d))
+  DERs <- matrix(nrow = n, ncol = length(d))
+  if (cov == F){
+    for (j in 1:n) {
+      DERs[j,] <-  MIXDER_function(r = r, d = d, L = L, Z.b = Z.b, eta0 = eta0_MC_var[j], eta1 = eta1_MC_var[j], sig0 = sig0_MC_var[j], kap = kap_MC_var[j])[, 2]
+      cat(paste("  Currently at Monte Carlo step:", toString(j), "of", 
+                toString(n)), sprintf('\r'))
+    }
+  }
+  else{
+    for (j in 1:n) {
+      DERs[j, ] <- MIXDER_function(r = r, L = L, Z.b = Z.b, d = d, eta0 = eta0_MC[j], eta1 = eta1_MC[j], sig0 = sig0_MC[j], kap = kap_MC[j])[, 2]
+      cat(paste("  Currently at Monte Carlo step:", toString(j), "of", 
+                toString(n)), sprintf('\r'))
+    }
+  }
+  for (i in 1:length(d)) {
+    sample_values <- sort(DERs[, i])
+    # Returning resulting CI
+    ninty_five_CI_lower[i] <- sample_values[ceiling((1 - 0.95) / 2 * n)] + background  #Need to add background prevalence of 0.00071 to every output
+    ninty_five_CI_upper[i] <- sample_values[(0.95 + (1 - 0.95) / 2) * n] + background
+  }
+
+  MIXDER$CI_lower = ninty_five_CI_lower
+  MIXDER$CI_upper = ninty_five_CI_upper
+  MIXDER$simpleeffect = IDER(d, ions = ions) + length(ions) * background #SCW: I believe we should add background prevalence to every IDER
+  names = colnames(MIXDER)
+  for (k in ions){
+    ider = IDER(d, ions = k)
+    MIXDER <- cbind(MIXDER, data.frame(ider))
+  }
+  colnames(MIXDER) <- c(names, ions)
+  if(graph == F){
+    return(MIXDER)
+  }
+  else{
+    CA <- MIXDER$CA
+    simpleeffect <- MIXDER$simpleeffect
+    plot(x = d * 100, y = CA * 100, type = "l", col = "red")
+    lines(x = d * 100, y = simpleeffect * 100, col = "black", lty = 2, lwd = 0.5)
+    for(i in 6:ncol(MIXDER)){
+      lines(x = d * 100, y = 100*as.vector(MIXDER[,i]), col = "green")
+    }
+    lines(x= d*100 , y = MIXDER$CI_upper * 100, lty = 'dashed', col = 'red')
+    lines(x= d*100 , y = MIXDER$CI_lower * 100, lty = 'dashed', col = 'red')
+    polygon(c(d*100,rev(d*100)),c(MIXDER$CI_lower * 100, rev(MIXDER$CI_upper * 100)),col = rgb(1, 0, 0,0.5), border = NA)
+  }
+}
+
+
+monte_carlo(c("Si", "Fe600"))
+monte_carlo(c("Si", "Fe600"), cov = F)
+monte_carlo(c("O", "Si", "Ti", " Fe600", "Fe450", "Fe300"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 start_time <- Sys.time()
 #MIXDER of 2-ion (Silicon and Fe600) with 60 different dosage points and Monte Carlo Ribbon Plot using covariances
 d1 = c(seq(0, 0.009, 0.001), seq(0.01, 0.5, by = 0.01))
@@ -239,7 +339,7 @@ Sys.time() - start_time
 
 two_ion_MIXDER$CI_lower = ninty_five_CI_lower
 two_ion_MIXDER$CI_upper = ninty_five_CI_upper
-two_ion_MIXDER$simpleeffect = IDER(d = 0.5*d1, L = 100, Z.b = 690) + IDER(d = 0.5*d1, L = 175, Z.b = 1075) + 0.00071
+two_ion_MIXDER$simpleeffect = IDER(d = 0.5*d1, L = 100, Z.b = 690) + IDER(d = 0.5*d1, L = 175, Z.b = 1075) + 0.00071 #SCW: I think here we should add background prevalence to every IDER instead (although this won't change the result much)
 two_ion_MIXDER$silicon = IDER(d = d1, L = 100, Z.b = 690) + 0.00071
 two_ion_MIXDER$ironsix = IDER(d = d1, L = 175, Z.b = 1075) + 0.00071
 #The graphing part
@@ -303,7 +403,7 @@ silicon1 <- two_ion_MIXDER_var$silicon
 ironsix1 <- two_ion_MIXDER_var$ironsix
 plot(x = d1 * 100, y = CA1 * 100, type = "l", col = "red")
 lines(x = d1 * 100, y = simpleeffect1 * 100, col = "black", lty = 2, lwd = 0.5)
-lines(x = d1 * 100, y = silicon1* 100, col = "green")  
+lines(x = d1 * 100, y = silicon1* 100, col = "green")
 lines(x = d1 * 100, y = ironsix1* 100, col = "green")
 lines(x= d1*100 , y = two_ion_MIXDER_var$CI_upper * 100, lty = 'dashed', col = 'red')
 lines(x= d1*100 , y = two_ion_MIXDER_var$CI_lower * 100, lty = 'dashed', col = 'red')
